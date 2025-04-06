e1a96f714597bf2ca42e4c4a463b84487a68eb3c
Константин Витковский <kon.vitkovskii@gmail.com>
2025-04-06 17:22:02 +0600

    haha

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -98,9 +98,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-    'DEFAULT_FILTER_BACKENDS': [
-        'django_filters.rest_framework.DjangoFilterBackend',
-    ],
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
